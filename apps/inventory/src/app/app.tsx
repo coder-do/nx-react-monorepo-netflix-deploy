d1d07ba2b983c0c16c13312c2936f6782614e842
Meruzh <meruzh.kiloyan@mail.ru>
2024-04-11 13:15:49 +0400

    some commit message

---
+++ apps/inventory/src/app/app.tsx
@@ -0,0 +1,10 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { ProductList } from '@react-monorepo/products';
+
+export function App() {
+  return (
+    <ProductList />
+  );
+}
+
+export default App;
